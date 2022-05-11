37d74abbfa9bb4dd292348fc765d38939dac27a0
21ciprian <2021ciprianoffice@gmail.com>
2022-05-11 12:41:04 +0100

    Added css file to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,40 +1,17 @@
 import React from 'react'
-import styled from 'styled-components'
+import styles from './Navbar.module.css'
 
-const Nav = styled.nav`
-	height: 50px;
-	background-color: #003580;
-	display: flex;
-	justify-content: center;
-`
-const NavSection = styled.section`
-	width: 100%;
-	max-width: 1024px;
-	padding: 0 0.5rem;
-	color: #fff;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-`
-const Button = styled.button`
-	border: none;
-	outline: none;
-	margin-left: 1.5rem;
-	padding: 0.5rem 1rem;
-	cursor: pointer;
-	color: #003580;
-`
 function Navbar() {
 	return (
-		<Nav>
-			<NavSection>
-				<span>Logo</span>
-				<div>
-					<Button>Register</Button>
-					<Button>Login</Button>
+		<nav className={styles.navbar}>
+			<section className={styles.navContainer}>
+				<span className={styles.logo}>Logo</span>
+				<div className={styles.navItems}>
+					<button className={styles.navButton}>Register</button>
+					<button className={styles.navButton}>Login</button>
 				</div>
-			</NavSection>
-		</Nav>
+			</section>
+		</nav>
 	)
 }
 
