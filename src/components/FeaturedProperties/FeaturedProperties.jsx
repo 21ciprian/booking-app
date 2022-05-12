ed8f3e36997200e61700abe110dde297e9dd1104
21ciprian <2021ciprianoffice@gmail.com>
2022-05-13 00:15:26 +0100

    Added featuredProperties component

--- src/components/FeaturedProperties/FeaturedProperties.jsx
+++ src/components/FeaturedProperties/FeaturedProperties.jsx
@@ -1,7 +1,67 @@
 import React from 'react'
+import styles from './FeaturedProperties.module.css'
 
 function FeaturedProperties() {
-	return <div>FeaturedProperties</div>
+	return (
+		<article className={styles.fprop}>
+			<section className={styles.fpItem}>
+				<img
+					className={styles.fpImg}
+					src='https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/26829941.jpg?k=d148213bb2c599ee7365806c432ec7ffaa54a55ff1c1219e1e9afb01779269a4&o=&hp=1'
+					alt='madrid'
+				/>
+				<span className={styles.fpName}>Madrid Rental Flats</span>
+				<span className={styles.fpCity}>Madrid</span>
+				<span className={styles.fpPrice}>£50</span>
+				<div className={styles.fpRating}>
+					<button>4.5</button>
+					<span>Great</span>
+				</div>
+			</section>
+			<section className={styles.fpItem}>
+				<img
+					className={styles.fpImg}
+					src='https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/183807379.jpg?k=da6c99a2a6c4620132fed17217aef48d0e1c9cbe482df3edc454d566fa197e3b&o=&hp=1'
+					alt='madrid'
+				/>
+				<span className={styles.fpName}>Madrid Rental Flats</span>
+				<span className={styles.fpCity}>Madrid</span>
+				<span className={styles.fpPrice}>£50</span>
+				<div className={styles.fpRating}>
+					<button>4.5</button>
+					<span>Great</span>
+				</div>
+			</section>
+			<section className={styles.fpItem}>
+				<img
+					className={styles.fpImg}
+					src='https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/13380634.jpg?k=7c7d515ae57a66fb2c2b928fc4c77453803aa97935c0a86c2e64bbfbf7c203b2&o=&hp=1'
+					alt='madrid'
+				/>
+				<span className={styles.fpName}>Madrid Rental Flats</span>
+				<span className={styles.fpCity}>Madrid</span>
+				<span className={styles.fpPrice}>£50</span>
+				<div className={styles.fpRating}>
+					<button>4.5</button>
+					<span>Great</span>
+				</div>
+			</section>
+			<section className={styles.fpItem}>
+				<img
+					className={styles.fpImg}
+					src='https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/26830011.jpg?k=b657679d464f57d814f58bcbf85c5b2d4f643924dcd174f76b5db63cc41e64e0&o=&hp=1'
+					alt='madrid'
+				/>
+				<span className={styles.fpName}>Madrid Rental Flats</span>
+				<span className={styles.fpCity}>Madrid</span>
+				<span className={styles.fpPrice}>£50</span>
+				<div className={styles.fpRating}>
+					<button>4.5</button>
+					<span>Great</span>
+				</div>
+			</section>
+		</article>
+	)
 }
 
 export default FeaturedProperties
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Featured from '../../components/Featured/Featured'
+import FeaturedProperties from '../../components/FeaturedProperties/FeaturedProperties'
 import Header from '../../components/Header/Header'
 import Navbar from '../../components/Navbar/Navbar'
 import PropertyList from '../../components/PropertyList/PropertyList'
@@ -15,6 +16,7 @@ function Home() {
 				<h1 className={styles.homeTitle}>Browse by property type</h1>
 				<PropertyList />
 				<h1 className={styles.homeTitle}>Homes guests love</h1>
+				<FeaturedProperties />
 			</section>
 		</div>
 	)
