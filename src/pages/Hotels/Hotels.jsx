8ba05425cf791da21ef0ffcda93a21e8f911fefb
21ciprian <2021ciprianoffice@gmail.com>
2022-05-14 23:32:01 +0100

    Added css file to Hotels component

--- src/pages/Hotels/Hotels.jsx
+++ src/pages/Hotels/Hotels.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Navbar from '../../components/Navbar/Navbar'
+import styles from './Hotels.module.css'
 
 function Hotels() {
 	return (
-		<div>
+		<section>
 			<Navbar />
 			<Header type='hotels' />
-		</div>
+			<section className={styles.hotelsContainer}>
+				<section hotelsWrapper>
+					<div className={styles.hotelsSearch}>
+						<h1 className={styles.hotelsTitle}>Search</h1>
+					</div>
+					<div className={styles.hotelsResult}></div>
+				</section>
+			</section>
+		</section>
 	)
 }
 
