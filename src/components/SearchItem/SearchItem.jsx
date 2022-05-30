147064287a641bad4a94c1d0cb260d88b4642857
21ciprian <2021ciprianoffice@gmail.com>
2022-05-30 13:35:38 +0100

    Displayed data from the item prop in SearchItem component

--- src/components/SearchItem/SearchItem.jsx
+++ src/components/SearchItem/SearchItem.jsx
@@ -1,36 +1,35 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import styles from './SearchItem.module.css'
 
-function SearchItem() {
+function SearchItem({item}) {
 	return (
 		<section className={styles.searchItem}>
-			<img
-				className={styles.siImg}
-				src='https://q-xx.bstatic.com/xdata/images/xphoto/max500/144066451.jpg?k=0a275cc3ef77a24d68bb26c8c5c39aa662271779cace3b0fbe53da08b6507581&o='
-				alt='Windsor Palace'
-			/>
+			<img className={styles.siImg} src={item.photos[0]} alt='Windsor Palace' />
 			<div className={styles.siDesc}>
-				<h1 className={styles.siTitle}> Westmister Appartments</h1>
-				<span className={styles.siDistance}>500m from center</span>
+				<h1 className={styles.siTitle}>{item.name}</h1>
+				<span className={styles.siDistance}>{item.distance}m from center</span>
 				<span className={styles.siTaxiOp}>Free airport taxi</span>
 				<span className={styles.siSubtitle}>Breakfast included</span>
-				<span className={styles.Features}>
-					Duplex • 2 bathrooms • kingsize bed
-				</span>
+				<span className={styles.siFeatures}>{item.desc}</span>
 				<span className={styles.siCancelOp}>Free cancellation</span>
 				<span className={styles.siCancelOpSubtitle}>
 					You can cancel your rezervation within 24hours of arrival
 				</span>
 			</div>
 			<div className={styles.siDetails}>
-				<div className={styles.siRating}>
-					<span>Excellent</span>
-					<button>8.9</button>
-				</div>
+				{item.rating && (
+					<div className={styles.siRating}>
+						<span>Excellent</span>
+						<button>{item.rating}</button>
+					</div>
+				)}
 				<div className={styles.siDetailsTexts}>
-					<span className={styles.siPrice}>£123</span>
+					<span className={styles.siPrice}>£{item.cheapestPrice}</span>
 					<span className={styles.siTaxOp}>Includes taxes and fees</span>
-					<button className={styles.siCheckButton}>See Availability</button>
+					<Link to={`/hotels/${item._id}`}>
+						<button className={styles.siCheckButton}>See Availability</button>
+					</Link>
 				</div>
 			</div>
 		</section>
