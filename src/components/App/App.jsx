af1240bd90dd1c3217879aa544e162a319df1714
21ciprian <2021ciprianoffice@gmail.com>
2022-06-02 00:52:13 +0100

    Created Login route in App component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from '../../pages/Home/Home'
 import Hotel from '../../pages/Hotel/Hotel'
 import Hotels from '../../pages/Hotels/Hotels'
+import Login from '../../pages/Login/Login'
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 					<Route path='/' element={<Home />} />
 					<Route path='/hotels' element={<Hotels />} />
 					<Route path='/hotels/:id' element={<Hotel />} />
+					<Route path='/login' element={<Login />} />
 				</Routes>
 			</div>
 		</Router>
