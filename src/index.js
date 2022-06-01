dbc8bd8e0fb9ccf88f7b2f3f6f462a446c3703e8
21ciprian <2021ciprianoffice@gmail.com>
2022-06-02 00:25:26 +0100

    Wrapped the App with AuthContextProvider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App/App'
+import { AuthCOntextProvider } from './context/AuthContext'
 import { SearchCOntextProvider } from './context/SearchContext'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
@@ -8,9 +9,11 @@ import reportWebVitals from './reportWebVitals'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		<SearchCOntextProvider>
-			<App />
-		</SearchCOntextProvider>
+		<AuthCOntextProvider>
+			<SearchCOntextProvider>
+				<App />
+			</SearchCOntextProvider>
+		</AuthCOntextProvider>
 	</React.StrictMode>
 )
 
