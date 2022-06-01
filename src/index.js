5a83e25027a1168f0445563dc525074df93111df
21ciprian <2021ciprianoffice@gmail.com>
2022-06-01 18:24:35 +0100

    Wrapped App component with SearchContextProvider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App/App'
+import { SearchCOntextProvider } from './context/SearchContext'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		<App />
+		<SearchCOntextProvider>
+			<App />
+		</SearchCOntextProvider>
 	</React.StrictMode>
 )
 
