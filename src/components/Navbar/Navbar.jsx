01c07f00011ac0ffcf49234a1cb5ed34ddd1ab95
21ciprian <2021ciprianoffice@gmail.com>
2022-06-02 02:02:26 +0100

    Display username on homepage if the user is authenticated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {Link} from 'react-router-dom'
+import {AuthContext} from '../../context/AuthContext'
 import styles from './Navbar.module.css'
 
 function Navbar() {
+	const {user} = useContext(AuthContext)
+
 	return (
 		<nav className={styles.navbar}>
 			<section className={styles.navContainer}>
 				<Link to='/' style={{color: 'inherit', textDecoration: 'none'}}>
 					<span className={styles.logo}>Logo</span>
 				</Link>
-				<div className={styles.navItems}>
-					<button className={styles.navButton}>Register</button>
-					<button className={styles.navButton}>Login</button>
-				</div>
+				{user ? (
+					user.username
+				) : (
+					<div className={styles.navItems}>
+						<button className={styles.navButton}>Register</button>
+						<button className={styles.navButton}>Login</button>
+					</div>
+				)}
 			</section>
 		</nav>
 	)
