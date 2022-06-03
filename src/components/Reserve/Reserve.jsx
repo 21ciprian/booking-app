951c524f42b8675377c45aada66ad87a70efb360
21ciprian <2021ciprianoffice@gmail.com>
2022-06-03 18:11:51 +0100

    Created Reserve component

---
+++ src/components/Reserve/Reserve.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styles from './Reserve.module.css'
+function Reserve() {
+	return <section className={styles.reserve}>Reserve</section>
+}
+
+export default Reserve
