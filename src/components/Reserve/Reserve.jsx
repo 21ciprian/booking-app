88e5a6bd2518d7eca2cf7faf880c70c49a10ab1c
21ciprian <2021ciprianoffice@gmail.com>
2022-06-03 18:25:58 +0100

    Added close button in Reserve component

--- src/components/Reserve/Reserve.jsx
+++ src/components/Reserve/Reserve.jsx
@@ -1,8 +1,20 @@
+import {faCircleXmark} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import React from 'react'
 import styles from './Reserve.module.css'
 
 function Reserve({setOpen, hotelId}) {
-	return <section className={styles.reserve}>Reserve</section>
+	return (
+		<section className={styles.reserve}>
+			<section className={styles.rContainer}>
+				<FontAwesomeIcon
+					icon={faCircleXmark}
+					className={styles.rClose}
+					onClick={() => setOpen(false)}
+				/>
+			</section>
+		</section>
+	)
 }
 
 export default Reserve
